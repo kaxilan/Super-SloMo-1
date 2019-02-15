83473ed2dd9be8fc58bbf292ea55f56bad2250b6
205418367 <47652945+205418367@users.noreply.github.com>
2019-02-15 18:22:29 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-MODELDIR="/home/ubuntu/Super-SloMo/model/SuperSloMo.ckpt"
+MODELDIR="/home/ubuntu/Super-SloMo/model"
 VIDEOSFOLDER="/home/ubuntu/Super-SloMo/data/custom/input"
 DATASETFOLDER="/home/ubuntu/Super-SloMo/data/custom/output"
-
+CHECKPOINT="/home/ubuntu/Super-SloMo/model/SuperSloMo.ckpt"
 
 #CUDA_VISIBLE_DEVICES='0' python3 -u data/create_dataset.py --videos_folder $VIDEOSFOLDER --dataset_folder $DATASETFOLDER
-CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset_root $DATASETFOLDER --checkpoint_dir $MODELDIR 2>&1 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset_root $DATASETFOLDER --checkpoint_dir $MODELDIR --checkpoint $CHECKPOINT 2>&1 &
 
 
